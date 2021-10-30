904c0104cd4f778a22dc98eb2eec9eed7ec225b9
Shayan Moazzam <Shayan Moazzam@DESKTOP-DQJPAJH>
2021-10-30 23:44:21 +0530

    First Commit

---
+++ Dproject/src/test/StudentAddress.java
@@ -0,0 +1,15 @@
+package test;
+
+public class StudentAddress {
+public String hNo,sName,city;
+public int pinCode;
+	public void getStuAddress()
+	{
+
+		System.out.println("hNo="+hNo);
+		System.out.println("sName="+sName);
+		System.out.println("city="+city);
+		System.out.println("pinCode="+pinCode);
+	}
+
+}
---
+++ Dproject/src/test/StudentContact.java
@@ -0,0 +1,14 @@
+package test;
+
+public class StudentContact {
+	
+	public long phoneNo;
+	public String mailId;
+	
+public void getStuContact()
+{
+	System.out.println("phoneNo"+phoneNo);
+	System.out.println("mailId"+mailId);
+}
+
+}
